6f9833070db6bdeb0855bbe4cb5ba6cfefb089d6
Francisco Calaça Xavier <chicocx@gmail.com>
2017-05-29 18:45:47 -0300

    first commit

---
+++ dht11.ino
@@ -0,0 +1,30 @@
+#include <DHT.h>
+ 
+#define DHTPIN A1 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+ 
+DHT dht(DHTPIN, DHTTYPE);
+ 
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("DHTxx test!");
+  dht.begin();
+}
+ 
+void loop() 
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(t) || isnan(h))   {
+    Serial.println("Failed to read from DHT");
+  } 
+  else  {
+    Serial.print("Umidade: ");
+    Serial.print(h);
+    Serial.print(" %t");
+    Serial.print("Temperatura: ");
+    Serial.print(t);
+    Serial.println(" *C");
+  }
+}
